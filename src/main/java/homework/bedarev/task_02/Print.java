60c9124bd2de12afc3bf736f0a360eb58ef44042
mj <madjo85@yandex.ru>
2018-03-27 18:18:15 +0300

    start 2 task ++ comlete copy class + Print

---
+++ src/main/java/homework/bedarev/task_02/Print.java
@@ -0,0 +1,11 @@
+package homework.bedarev.task_02;
+
+public class Print {
+
+    public Print() {
+    }
+
+    public void printMsg(String message) {
+        System.out.println(message);
+    }
+}
--- src/main/java/homework/bedarev/task_02/ReflectionFieldCopier.java
+++ src/main/java/homework/bedarev/task_02/ReflectionFieldCopier.java
@@ -3,11 +3,12 @@ package homework.bedarev.task_02;
 import homework.common.BeanFieldCopier;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ReflectionFieldCopier implements BeanFieldCopier {
+    Print print = new Print();
+
     @Override
     public void copy(Object from, Object to) {
         Class classFrom = from.getClass();
@@ -18,37 +19,13 @@ public class ReflectionFieldCopier implements BeanFieldCopier {
             returnedValueGet = findGetter(classFrom, from);
             if (returnedValueGet.size() > 0) {
                 for (Map.Entry<String, Map<Class<?>, Object>> entry: returnedValueGet.entrySet()) {
-                    System.out.println("Name method: " + entry.getKey());
                     valueMap = entry.getValue();
                     valueMap.entrySet()
                             .stream()
                             .forEach(e -> {
-                                System.out.println(e.getValue());
                                 setValue(e.getValue(),e.getKey(),entry.getKey(), to);
                             });
                 }
-            }
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    private void setValue (Object valueToSet,Class<?> typeParam, String nameMethod, Object to) {
-        try {
-            Map<String, Class[]> setterMap = findSetter(to.getClass(), to);
-            System.out.println(typeParam.toString());
-            setterMap.entrySet()
-                    .removeIf( e -> !e.getKey().equals(nameMethod));
-
-            Class<?> clazz = setterMap
-                    .entrySet()
-                    .iterator()
-                    .next()
-                    .getValue()[0];
-
-            if (clazz == typeParam) {
-                System.out.println("TypeTo: " + typeParam.toString());
-                setSetter(to, setterMap, valueToSet);
             }
         } catch (Exception exception) {
             exception.printStackTrace();
@@ -61,14 +38,34 @@ public class ReflectionFieldCopier implements BeanFieldCopier {
         for (Method method: methods) {
             if ((method.getName().startsWith("get")) && (!method.getName().equals("getClass"))) {
                 Map<Class<?>, Object> valueAndTypeFrom = new HashMap<>();
-                System.out.println("Name method: " + method.getName());
                 String nameMethodFrom = method.getName().substring(3);
                 Object returnValue = method.invoke(from, null);
                 valueAndTypeFrom.put(method.getReturnType(),returnValue);
                 getterFrom.put(nameMethodFrom, valueAndTypeFrom);
             }
         }
+        print.printMsg("Found " + getterFrom.size() + " getters");
         return  getterFrom;
+    }
+
+    private void setValue (Object valueToSet,Class<?> typeParam, String nameMethod, Object to) {
+        try {
+            Map<String, Class[]> setterMap = findSetter(to.getClass(), to);
+            setterMap.entrySet()
+                    .removeIf( e -> !e.getKey().equals(nameMethod));
+
+            Class<?> clazz = setterMap
+                    .entrySet()
+                    .iterator()
+                    .next()
+                    .getValue()[0];
+
+            if (clazz == typeParam) {
+                setValueToSetter(to, setterMap, valueToSet);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
     public Map<String, Class[]> findSetter(Class classTo, Object to) throws Exception{
@@ -85,9 +82,9 @@ public class ReflectionFieldCopier implements BeanFieldCopier {
         return setterTo;
     }
 
-    private static void setSetter(Object objectTo,
-                                  Map<String, Class[]> mapNameAndClass,
-                                  Object valueToSet) throws Exception{
+    private static void setValueToSetter(Object objectTo,
+                                         Map<String, Class[]> mapNameAndClass,
+                                         Object valueToSet) throws Exception{
 
         for (Map.Entry<String, Class[]> entry: mapNameAndClass.entrySet()) {
             Class ClassTypeParameter = entry.getValue()[0];
@@ -95,19 +92,4 @@ public class ReflectionFieldCopier implements BeanFieldCopier {
             method.invoke(objectTo, ClassTypeParameter.cast(valueToSet));
         }
     }
-
-    private Type getMethodParameter(Method method) {
-        Type[] types = method.getParameterTypes();
-        if (types.length == 1) {
-            return types[0];
-        }
-        return null;
-    }
-
-    private <T> boolean checkValues(Class<? extends T> from, Class<? super T> to) {
-        return true;
-    }
-
-
-
 }
